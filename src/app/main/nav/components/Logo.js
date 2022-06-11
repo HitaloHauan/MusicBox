775e6d3926d5deb9f03f2d9013f851a3fee24bd8
Hitalo Hauan <hitalodev@hotmail.com>
2022-06-11 03:16:51 -0300

    ajust margin logo

--- src/app/main/nav/components/Logo.js
+++ src/app/main/nav/components/Logo.js
@@ -5,9 +5,9 @@ import MusicBox from '../../../assets/icons/musicbox.png'
 function Logo() {
 
     return(
-        
+
         <div className='flex items-center' id='logo'>
-            <img alt='logo' src={MusicBox} style={{width: 60}} />
+            <img alt='logo' src={MusicBox} style={{width: 60}} className='mr-3' />
             <Typography fontSize={25} fontWeight='bold' >MusicBox</Typography>
         </div>
     )
